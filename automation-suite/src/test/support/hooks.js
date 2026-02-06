bae0ce3e4f276a2958be491450be35015f539450
Kulathunga D.M.D.T. <kulathungadmdt.21@uom.lk>
2026-02-06 20:10:53 +0530

    Updated hooks file

--- automation-suite/src/test/support/hooks.js
+++ automation-suite/src/test/support/hooks.js
@@ -1,11 +1,37 @@
-const { Before, After } = require('@cucumber/cucumber');
+const { Before, After, BeforeAll, AfterAll } = require('@cucumber/cucumber');
+const { chromium } = require('@playwright/test');
+
+let browser;
+
+
+BeforeAll(async function () {
+    browser = await chromium.launch({ 
+        headless: false 
+    });
+});
 
 Before(async function () {
     await this.init(); 
+
+    this.context = await browser.newContext();
+    this.page = await this.context.newPage(); 
 });
 
 After(async function () {
     if (this.request) {
         await this.request.dispose();
+    }
+
+    if (this.page) {
+        await this.page.close();
+    }
+    if (this.context) {
+        await this.context.close();
+    }
+});
+
+AfterAll(async function () {
+    if (browser) {
+        await browser.close();
     }
 });
